6637b34ce794cbe8fc226c197ed219826a6a86db
Liberess <58577796+Liberess@users.noreply.github.com>
2021-10-29 02:07:03 +0000

    Game클래스화

---
+++ Game.h
@@ -0,0 +1,24 @@
+#ifndef __Game_H__
+#define __Game_H__
+
+#include "SDL.h"
+
+class Game
+{
+public:
+  Game() {}
+  ~Game() {}
+  bool Init(const char* title, int x, int y, int w, int h, int flags);
+  void Render();
+  void Update();
+  bool Running() const { return m_bRunning; }
+  void HandleEvents();
+  void Clean();
+
+private:
+  SDL_Window* m_pWindow;
+  SDL_Renderer* m_pRenderer;
+  bool m_bRunning;
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,71 +1,20 @@
-#include <SDL.h>
+#include "Game.h"
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-bool g_bRunning = false; //게임 중? 아님?
-
-int main(int argc, char* args[]);
-bool Init(const char* title, int x, int y, int w, int h, int flags);
-void Update();
-void Render();
+Game* g_game = 0;
 
 int main(int argc, char* args[])
 {
-  if(Init("Breaking Up HelloSDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN))
-  {
-    g_bRunning = true;
-  }
-  else
-  {
-    return 1;
-  }
+  g_game = new Game();
+  g_game->Init("Game Class", 100, 100, 640, 480, 0);
 
-  while(g_bRunning)
+  while (g_game->Running())
   {
-    Update();
-    Render();
-    SDL_Delay(1000);
+    g_game->HandleEvents();
+    g_game->Update();
+    g_game->Render();
   }
-
-  SDL_Quit();
+  
+  g_game->Clean();
 
   return 0;
-}
-
-bool Init(const char* title, int x, int y, int w, int h, int flags)
-{
-  if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-  {
-    g_pWindow = SDL_CreateWindow(title, x, y, w, h, flags);
-
-    if (g_pWindow != 0)
-    {
-      g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-    }
-  }
-  else
-  {
-    return 1;
-  }
-
-  SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 0);
-}
-
-void Update()
-{
-  static int count = 0;
-
-  if(count >= 3)
-  {
-    g_bRunning = false;
-    return;
-  }
-
-  ++count;
-}
-
-void Render()
-{
-  SDL_RenderClear(g_pRenderer);
-  SDL_RenderPresent(g_pRenderer);
 }
